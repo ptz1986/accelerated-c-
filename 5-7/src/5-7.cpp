e4aa23e994a0e3568b66444f7d4b334ee2de0948
ptz <34560685+ptz1986@users.noreply.github.com>
2017-12-19 09:46:01 +0800

    5-7 code: split test

---
+++ 5-7/src/5-7.cpp
@@ -0,0 +1,77 @@
+//============================================================================
+// Name        : 5-7.cpp
+// Author      : ptz
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+vector<string> split(string& s)
+{
+	vector<string> vec;
+	string::size_type size = s.size();
+	string::size_type i = 0, j = 0;
+
+	while (i != size)
+	{
+		while (i!= size && isspace(s[i]))
+		{
+			i++;
+		}
+
+		j = i;
+
+		while (j!= size && !isspace(s[j]))
+		{
+			j++;
+		}
+
+		if (i != j)
+		{
+			vec.push_back(s.substr(i, j - i));
+			i = j;
+		}
+
+	}
+
+	for (vector<string>::const_iterator iter = vec.begin();
+				iter != vec.end(); iter++)
+	{
+		cout << "subroutine: " << *iter << endl;
+	}
+
+	return vec;
+}
+
+
+
+int main()
+{
+
+	string str;
+
+	while (getline(cin, str))
+	{
+		vector<string> v = split(str);
+
+		for (vector<string>::const_iterator iter = v.begin();
+				iter != v.end(); iter++)
+		{
+			cout << *iter << endl;
+		}
+	}
+
+
+	return 0;
+}
